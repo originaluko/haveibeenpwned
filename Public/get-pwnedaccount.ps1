dbaf2e45e6df1098ea9bf74fc725f61f617779c8
Mark Ukotic <ukotic@gmail.com>
2019-01-19 18:32:13 +1100

    Added UserAgent string to work with Have I Been Pwned v2 API

--- Public/get-pwnedaccount.ps1
+++ Public/get-pwnedaccount.ps1
@@ -15,6 +15,10 @@ Function Get-PwnedAccount
             Get-PwnedAccount -EmailAdddress email@domain.com
             Retuns all accounts that have been pwned via the supplied email address / username.
 
+            .EXAMPLE
+            Get-PwnedAccount -EmailAdddress email@domain.com -UserAgent "My User Agent"
+            Same as Example 1 but specifies a custom User Agent String.
+
             .INPUTS
             None
  
@@ -34,7 +38,10 @@ Function Get-PwnedAccount
     Param (
         [Parameter(Mandatory)]
         [ValidatePattern('(\w+@[]a-zA-Z_]+?\.[a-zA-Z]{2,6})')]
-        [string]$EmailAddress
+        [string]$EmailAddress,
+
+        [ValidatePattern('\w')]
+        [string]$UserAgent = “HaveIBeenPwned Powershell Module”
     )
 
 
@@ -47,7 +54,7 @@ Function Get-PwnedAccount
         [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
         try
         {
-            $Request = Invoke-RestMethod -Uri $URI
+            $Request = Invoke-RestMethod -Uri $URI -UserAgent $UserAgent
         }
          Catch [System.Net.WebException] {
             Switch ($_.Exception.Message) {
